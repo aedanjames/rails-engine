4a30522d93fba4403b5f872d7b66e1472408d793
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-28 16:47:34 -0600

    test: write test for merchants api can get one merhcant by it's id. Add show resource to routes, define show action in merchants controller. Pass test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -12,6 +12,8 @@ describe "Merchants API" do
 
     merchants = json[:data]
     expect(merchants.count).to eq(5)
+
+    expect(merchants).to be_an(Array)
     
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
@@ -28,5 +30,33 @@ describe "Merchants API" do
       expect(attributes).to have_key(:name)
       expect(attributes[:name]).to be_a(String)
     end 
+  end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+  
+    get "/api/v1/merchants/#{id}"
+  
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    merchant = json[:data]
+  
+    expect(response).to be_successful
+
+    expect(merchant).to be_an(Hash)
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to be_a(String)
+
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes]).to be_a(Hash)
+
+    attributes = merchant[:attributes]
+    expect(attributes).to have_key(:name)
+    expect(attributes[:name]).to be_a(String)
+
+    expect(merchant).to_not have_key(:items)
   end
 end
