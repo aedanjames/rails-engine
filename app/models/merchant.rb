b1fdaf54c35bc586f0a209d64a4563647f6ff8e1
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-28 14:48:04 -0600

    test: write relationship tests for models according to schema. Add relationship validations to models. Pass tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,9 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
+
+  has_many :items, dependent: :destroy
+  has_many :invoices, dependent: :destroy
+  has_many :invoice_items, through: :items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 end 
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do 
   describe 'relationships' do 
- 
+    it { should have_many :items }
+    it { should have_many :invoices }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
   end 
   
   describe 'validations' do 
