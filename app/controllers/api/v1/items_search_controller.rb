5483e8eb66383846184194a5db2bb05adc9a0fc1
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-30 10:46:41 -0600

    refactor: add conditionals to controllers to handle empty or missing requests. Postman harness tests pass for set one

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::ItemsSearchController < ApplicationController
   def find_all 
-    items = Item.multiple_items(params[:name])
-    if items 
-      render json: ItemSerializer.new(items)
-    elsif items.nil?
-      render json: { data: { message: 'Error: Item Not Found' } }
+    if params[:name].present?
+    items = Item.multiple_items(params[:name]) 
+      if items 
+        render json: ItemSerializer.new(items)
+      elsif items.nil?
+        render json: { data: { message: 'Error: Item Not Found' } }
+      end 
+    elsif params[:name].nil? || params[:name].empty?
+      render status: 400
     end 
   end
 end 
--- app/controllers/api/v1/merchants_search_controller.rb
+++ app/controllers/api/v1/merchants_search_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::MerchantsSearchController < ApplicationController
   def find
+    if params[:name].present?
     merchant = Merchant.single_merchant(params[:name])
-    if merchant
-    render json: MerchantSerializer.new(merchant)
-    elsif merchant.nil?
-      render json: { data: { message: 'Error: Merchant Not Found' } }
+      if merchant.nil? 
+        render json: { data: { message: 'Error: Merchant Not Found' } }
+      else 
+        render json: MerchantSerializer.new(merchant)
+      end 
+    elsif params[:name].nil? || params[:name].empty?
+      render status: 400
     end
   end
 end 
