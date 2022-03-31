307b745b534862151dcf65a523e5045d4313aa7d
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-30 19:13:22 -0600

    test: write test for item search api endpoint can find one item from a partial case-insensitive search. Add route, add action to controller. Pass test

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -11,4 +11,17 @@ class Api::V1::ItemsSearchController < ApplicationController
       render status: 400
     end 
   end
+
+  def find 
+    if params[:name].present?
+    items = Item.one_item(params[:name]) 
+      if items 
+        render json: ItemSerializer.new(items)
+      elsif items.nil?
+        render json: { data: { message: 'Error: Item Not Found' } }
+      end 
+    elsif params[:name].nil? || params[:name].empty?
+      render status: 400
+    end 
+  end
 end 
