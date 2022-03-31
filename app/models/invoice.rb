13457128f852c825f9da9cab00dfa7dc64955cc3
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-31 17:35:21 -0600

    test: write test for model instance method 'only_item_on_invoice. Define method pass test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,4 +7,8 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :transactions
   has_many :items, through: :invoice_items
+
+  def only_item_on_invoice?(item)
+    items.count == 1 && items.first.id == item.id
+  end 
 end 
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe Invoice, type: :model do
   describe 'validations' do 
     it { should validate_presence_of :status }
   end 
+
+  describe 'instance methods' do 
+    describe '.only_item_on_invoice' do 
+      it 'returns a boolean whether an item is the only item on an invoice' do 
+        customer = create(:customer)
+        merchant = create(:merchant)
+        item = create(:item, merchant_id: merchant.id)
+        invoice = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: "shipped")
+        joins = InvoiceItem.create!(invoice_id: invoice.id, item_id: item.id, quantity: 1, unit_price: 10)
+        expect(invoice.only_item_on_invoice?(item)).to eq(true)
+        item_2 = create(:item, merchant_id: merchant.id)
+        joins = InvoiceItem.create!(invoice_id: invoice.id, item_id: item_2.id, quantity: 1, unit_price: 10)
+        expect(invoice.only_item_on_invoice?(item)).to eq(false)
+      end 
+    end 
+  end
 end 
