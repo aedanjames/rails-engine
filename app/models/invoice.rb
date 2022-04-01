773fabc88e4eba4b9b425fb6fb87c90a85e6ca80
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-31 18:41:56 -0600

    test: write test for class method delete_invoices. Define class method in invoice class. Pass test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,11 +4,19 @@ class Invoice < ApplicationRecord
   belongs_to :customer 
   belongs_to :merchant 
 
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :transactions
   has_many :items, through: :invoice_items
 
   def only_item_on_invoice?(item)
     items.count == 1 && items.first.id == item.id
-  end 
+  end
+
+  def self.delete_invoices(invoices, item)
+    invoices.each do |invoice|
+      if invoice.only_item_on_invoice?(item)
+        invoice.destroy
+      end 
+    end 
+  end
 end 
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -28,4 +28,19 @@ RSpec.describe Invoice, type: :model do
       end 
     end 
   end
+
+  describe 'class methods' do 
+    describe '#delete_invoices' do 
+      it 'iterates through an array of invoices and deletes them if they only have one item (which is also being deleted)' do
+        customer = create(:customer)
+        merchant = create(:merchant)
+        item = create(:item, merchant_id: merchant.id)
+        invoice = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: "shipped")
+        joins = InvoiceItem.create!(invoice_id: invoice.id, item_id: item.id, quantity: 1, unit_price: 10)
+        expect(Invoice.all).to eq([invoice])
+        Invoice.delete_invoices([invoice], item)
+        expect(Invoice.all).to eq([])
+      end 
+    end 
+  end 
 end 
